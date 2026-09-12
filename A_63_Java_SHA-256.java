ffbbf1d25b5ea19fa802393bf412d78c4cda133b
hemalesh250141-CSE <hemalesh.250141@cse.ritchennai.edu.in>
2026-09-12 21:01:45 +0530

    Create A_63_Java_SHA-256.java

---
+++ A_63_Java_SHA-256.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.security.MessageDigest;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        sc.close();
+        
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] digest = md.digest(s.getBytes());
+            
+            StringBuilder sb = new StringBuilder();
+            for (byte b : digest) {
+                sb.append(String.format("%02x", b));
+            }
+            
+            System.out.println(sb.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
